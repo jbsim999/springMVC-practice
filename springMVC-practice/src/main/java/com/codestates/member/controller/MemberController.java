36e9e3e47a3d7afe1cd3a5cbeb86b32d95e4b495
jbsim999 <junbo1218@gmail.com>
2022-12-28 00:28:35 +0900

    version7 commit conclude exceptionHandler in Spring MVC

--- springMVC-practice/src/main/java/com/codestates/member/controller/MemberController.java
+++ springMVC-practice/src/main/java/com/codestates/member/controller/MemberController.java
@@ -5,9 +5,13 @@ import com.codestates.member.dto.MemberPostDto;
 import com.codestates.member.dto.MemberResponseDto;
 import com.codestates.member.entity.Member;
 import com.codestates.member.service.MemberService;
+import com.codestates.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,8 +22,9 @@ import java.util.stream.Collectors;
 import com.codestates.member.mapper.MemberMapper;
 
 @RestController
-@RequestMapping("/v5/members")
+@RequestMapping("/v7/members")
 @Validated
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -39,9 +44,7 @@ public class MemberController {
 //        member.setPhone(memberPostDto.getPhone());
 
         Member member = mapper.memberPostDtoToMember(memberPostDto);
-
         Member response = memberService.createMember(member);
-
         return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.CREATED);
     }
 
@@ -50,14 +53,12 @@ public class MemberController {
                                       @Valid @RequestBody MemberPatchDto memberPatchDto){
         memberPatchDto.setMemberId(memberId);
 //        memberPatchDto.setName("홍길동");
-
 //        Member member = new Member();
 //        member.setMemberId(memberPatchDto.getMemberId());
 //        member.setName(memberPatchDto.getName());
 //        member.setPhone(memberPatchDto.getPhone());
 
         Member response = memberService.updateMember(mapper.memberPatchDtoToMember(memberPatchDto));
-
         return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),HttpStatus.OK);
     }
 
@@ -66,7 +67,6 @@ public class MemberController {
 //        System.out.println("# memberId : " + memberId);
 
         Member response = memberService.findMember(memberId);
-
         return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),HttpStatus.OK);
     }
 
@@ -75,7 +75,6 @@ public class MemberController {
 //        System.out.println("# get Members");
 
         List<Member> members = memberService.findMembers();
-
         List<MemberResponseDto> response =
                 members.stream()
                         .map(member -> mapper.memberToMemberResponseDto(member))
@@ -86,8 +85,22 @@ public class MemberController {
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id")long memberId){
         System.out.println("# delete member");
-
         memberService.deleteMember(memberId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+/*
+    @ExceptionHandler
+    public ResponseEntity handleException(MethodArgumentNotValidException e){
+        final List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<ErrorResponse.FieldError> errors =
+                fieldErrors.stream()
+                        .map(fieldError -> new ErrorResponse.FieldError(
+                                fieldError.getField(),
+                                fieldError.getRejectedValue(),
+                                fieldError.getDefaultMessage()))
+                        .collect(Collectors.toList());
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
+    }
+ */
+
 }
